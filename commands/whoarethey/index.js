b38dc8fb6ff3ce4a1cbbcf8c19c18d6d903f15f0
Petko D. Petkov <pdp@websecurify.com>
2021-02-12 21:38:20 +0000

    Improved tool.

--- commands/whoarethey/index.js
+++ commands/whoarethey/index.js
@@ -1,40 +1,56 @@
 exports.yargs = {
     command: 'whoarethey <accounts...>',
     describe: 'find social networking accounts and more',
+    aliases: ['whothey', 'who'],
 
     builder: {
         concurrency: {
             describe: 'Number of concurrent requests',
             type: 'number',
             default: 100,
-            alias: 'c'
+            alias: ['c']
         },
 
         categories: {
             describe: 'Only use selected categories',
             type: 'array',
             default: [],
-            alias: 't'
+            alias: ['t']
+        },
+
+        outputFormat: {
+            describe: 'Output format for results',
+            type: 'string',
+            default: 'table',
+            alias: ['o', 'output'],
+            choices: ['table', 'json', 'url']
         }
     },
 
     handler: async(argv) => {
-        const { concurrency, categories, accounts } = argv
+        const { concurrency, categories, outputFormat, accounts } = argv
 
         const { WhoAreThey } = require('../../lib/whoarethey')
 
         const w = new WhoAreThey({ maxConcurrent: concurrency })
 
-        w.on('log', console.log)
-        w.on('info', console.info)
-        w.on('warn', console.warn)
-        w.on('error', console.error)
-        w.on('debug', console.debug)
-
         for (const account of accounts) {
             const results = await w.fingerprint(account, { categories })
 
-            console.table(results)
+            if (outputFormat === 'table') {
+                console.table(results)
+            }
+            else
+            if (outputFormat === 'json') {
+                console.log(JSON.stringify(results))
+            }
+            else
+            if (outputFormat === 'url') {
+                results.forEach(({ prettyUri }) => console.log(prettyUri))
+            }
+            else {
+                throw new Error(`Unexepcted state`)
+            }
         }
     }
 }
